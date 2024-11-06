d734518a709a69d32bd8076c1fc8106db993368d
MGahelrasoul <m97gahelrasoul@gmail.com>
2024-11-05 21:02:47 -0500

    Skybox and basic folder structure implemented

--- customers/NavigationNPC.gd
+++ customers/NavigationNPC.gd
@@ -9,7 +9,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		random_position.z = randf_range(-5.0, 5.0)
 		navigation_agent_3d.set_target_position(random_position)
 		
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var destination = navigation_agent_3d.get_next_path_position()
 	var local_destination = destination - global_position
 	var direction = local_destination.normalized()
