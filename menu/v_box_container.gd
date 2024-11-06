0bb71a0e280562bd3119085da623ee07fb731850
MGahelrasoul <m97gahelrasoul@gmail.com>
2024-11-06 13:24:24 -0500

    menu initialized

---
+++ menu/v_box_container.gd
@@ -0,0 +1,12 @@
+extends VBoxContainer
+
+const NAVIGATION_LEVEL = preload("res://NavigationLevel.tscn")
+
+func _on_new_game_button_pressed() -> void:
+	get_tree().change_scene_to_packed(NAVIGATION_LEVEL)
+
+func _on_load_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
