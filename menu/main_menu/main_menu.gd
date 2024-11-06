208dc01e9f6297c684f240183f77dab4aeecc8c4
MGahelrasoul <m97gahelrasoul@gmail.com>
2024-11-06 16:59:16 -0500

    file system organization update

--- menu/main_menu/main_menu.gd
+++ menu/main_menu/main_menu.gd
@@ -1,11 +1,11 @@
-extends VBoxContainer
+extends Control
 
 const NAVIGATION_LEVEL = preload("res://game.tscn")
 
-func _on_new_game_button_pressed() -> void:
+func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_packed(NAVIGATION_LEVEL)
 
-func _on_load_button_pressed() -> void:
+func _on_options_button_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_quit_button_pressed() -> void:
---
+++ menu/pause_menu/pause_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+#const NAVIGATION_LEVEL = preload("res://game.tscn")
+
+func _on_resume_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
