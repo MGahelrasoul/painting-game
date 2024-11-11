1b48784992447bc7253c4b4b46ecaa7a89eaffe7
MGahelrasoul <m97gahelrasoul@gmail.com>
2024-11-10 19:14:35 -0500

    Options ui implementation

--- menu/main_menu/main_menu.gd
+++ menu/main_menu/main_menu.gd
@@ -2,11 +2,13 @@ extends Control
 
 const NAVIGATION_LEVEL = preload("res://game.tscn")
 
-func _on_play_button_pressed() -> void:
+@onready var main = $"../"
+
+func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_packed(NAVIGATION_LEVEL)
 
 func _on_options_button_pressed() -> void:
-	pass # Replace with function body.
+	main.settingsMenu()
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
---
+++ menu/settings/settings.gd
@@ -0,0 +1,37 @@
+extends Control
+
+@onready var main = $"../"
+
+# Main Settings
+func _on_resolutions_options_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920, 1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1600, 900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
+
+func _on_fullscreen_options_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		1:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		2:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+
+func _on_v_sync_options_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+		1:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+
+# Controls Settings
+
+
+func _on_button_pressed() -> void:
+	main.settingsMenu()
